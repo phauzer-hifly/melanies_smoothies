fcfdcbf13647a488d313c464abf092ce82f02923
phauzer-hifly <peter.hauzer@hiflylabs.com>
2026-09-15 13:29:15 +0200

    Update session handling for Snowflake connection
    Replace Snowflake session retrieval with Streamlit connection.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -13,7 +12,8 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name of you smoothie will be:", name_on_order)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
@@ -37,3 +37,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon='✅')
+
+
